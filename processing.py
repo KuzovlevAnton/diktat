4b7b1fc1ddfbea0471b0d289dd9303e3070df174
anton <kuzovlev.anton58@gmail.com>
2025-06-08 23:55:58 +0300

    0806

--- processing.py
+++ processing.py
@@ -323,7 +323,7 @@ def processing(path, image_size_horisontal=54, white_thereshold_relative=0.65, c
     print(to_replace)
     print()
     for i in range(len(new_result)):
-        text += f"{str(i + 1) + "."} {new_result[i]}\n"
+        text += f"{str(i + 1)}. {new_result[i]}\n"
         print(str(i+1)+".", new_result[i])
 
     cv2.imwrite("ceils_field.png", img)
